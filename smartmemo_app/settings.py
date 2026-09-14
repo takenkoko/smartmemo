351ff53291c406a8b588c2ee98507239cfc97e98
Takeshi Satake <takeshi.satake.dev@gmail.com>
2026-09-14 16:25:10 +0900

    Migrate database to PostgreSQL and update security settings

--- smartmemo_app/settings.py
+++ smartmemo_app/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s&o6@&5ldly-7_oo*6e!@-q@3gbm9uo(pr*zc)vdp+ie*g61dk'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'smartmemo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'smartmemo',
+        'USER':'postgres',
+        'PASSWORD':config('POSTGRES_PASSWORD'),
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
